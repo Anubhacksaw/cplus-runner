beaaa746cf5087336ca775fce6370a971c539b06
Anubhab Mukherjee <78078306+Anubhab-ai@users.noreply.github.com>
2022-07-06 22:44:35 +0530

    Update compilerc++.sh

--- compilerc++.sh
+++ compilerc++.sh
@@ -19,13 +19,31 @@ com (){
 	else
 		break
 	fi
-	
+	echo "___Your Output file___"
+	./$a 
 }
+echo "\033[41;5m▄︻デC̷ P̷L̷U̷S̷ P̷L̷U̷S̷ R̷U̷N̷N̷E̷R̷══━一\033[0m"
 echo "Enter the file name: "
 read a 
-touch $a.cpp
 sf
-echo "___Your Output file___"
-./$a 
-echo "___exiting___"
-echo "Hii from Anubhab"
+while :
+do
+echo " "
+echo "--------------------------"
+echo "1. I want to edit my code"
+echo "2.Exit"
+echo "--------------------------"
+echo " "
+read c
+case $c in
+	1 )
+		sf
+		;;
+	2 )
+		echo "\033[42;5m _____Exiting_____\033[0m"
+		echo "\033[44;5m@Anubhab\033[0m"
+		echo "\033[33mhttps://github.com/Anubhab-ai\033[0m"
+		exit 0
+		;;
+esac
+done
